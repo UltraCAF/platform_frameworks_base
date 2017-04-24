12075347e65bf43857f21813dda1d547599cd403
Jake Weinstein <xboxlover360@gmail.com>
2017-04-24 19:40:08 -0400

    Revert "SystemUI: Add battery percentage for RJIL"
    * Kill CAF's battery styles because ours are better
    
    This reverts commit 24f2ff13f34b4b89f6c1e3bc5621bdcc41ec88b8.
    
    Change-Id: I49cdcc9e7b0e4cc89489f84c776cf74e90c29d2c
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
@@ -114,7 +114,6 @@ public class BatteryMeterDrawable extends Drawable implements
     private static final int ANIM_DURATION = 500;
     private int mAnimOffset;
     private boolean mCharging;
-    private boolean mBatteryPct;
 
     private final Runnable mInvalidate = new Runnable() {
         @Override
@@ -163,7 +162,6 @@ public class BatteryMeterDrawable extends Drawable implements
         }
         levels.recycle();
         colors.recycle();
-        mBatteryPct = context.getResources().getBoolean(R.bool.config_showBatteryPercentage);
         updateShowPercent();
         mWarningString = context.getString(R.string.battery_meter_very_low_overlay_symbol);
         mCriticalLevel = mContext.getResources().getInteger(
@@ -330,8 +328,8 @@ public class BatteryMeterDrawable extends Drawable implements
     }
 
     private void updateShowPercent() {
-        mShowPercent = (0 != Settings.System.getInt(mContext.getContentResolver(),
-                SHOW_PERCENT_SETTING, 0)) && !mBatteryPct;
+        mShowPercent = 0 != Settings.System.getInt(mContext.getContentResolver(),
+                SHOW_PERCENT_SETTING, 0);
     }
 
     private int getColorForLevel(int percent) {
